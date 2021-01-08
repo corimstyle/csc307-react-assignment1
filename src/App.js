daf6f23e79df36a6bfc10f350299914a2f00d28d
Andrew Mariano <corim.corim@gmail.com>
2021-01-08 15:25:30 -0800

    Add Form submit

--- src/App.js
+++ src/App.js
@@ -17,6 +17,12 @@ class App extends Component {
     });
   };
 
+  handleSubmit = (character) => {
+    this.setState({
+      characters: [...this.state.characters, character],
+    });
+  };
+
   render() {
     const { characters } = this.state;
 
@@ -26,7 +32,7 @@ class App extends Component {
           characterData={characters}
           removeCharacter={this.removeCharacter}
         />
-        <Form />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     );
   }
